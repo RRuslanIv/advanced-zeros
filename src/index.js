5fe137b74a8f4c123f0e70267f794f9dc921e122
RRuslanIv <r.rusachenka@gmail.com>
2019-03-21 15:22:57 +0300

    calculation quantity of zeros at the end of number which is the factorial of the number in the base system

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var n = 2;
+  var arr = []
+   while (n <= base) {
+         if (base % n == 0) {
+           arr.push(n)
+           base = base / n;
+         } else n++;
+    return arr.reverse()
+  }
+  
+  var count = 0
+  var rez = []
+  for ( let i = 0; i < arr.length; i++) {
+    let del = arr[i]
+    while (del <= number) {
+      count += Math.floor(number / del)
+      del *= arr[i]
+    } 
+    rez.push(count)
+  }
+  	return rez[0]
 }
