b5b625683b49be9bd6cef800328daf115905af97
RRuslanIv <r.rusachenka@gmail.com>
2019-03-22 16:27:24 +0300

    modify: add check for repeated chars in first array of base multipliers

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  var n = 2;
-  var arr = []
-   while (n <= base) {
-         if (base % n == 0) {
-           arr.push(n)
-           base = base / n;
-         } else n++;
-    return arr.reverse()
-  }
-  
-  var count = 0
-  var rez = []
-  for ( let i = 0; i < arr.length; i++) {
-    let del = arr[i]
-    while (del <= number) {
-      count += Math.floor(number / del)
-      del *= arr[i]
-    } 
-    rez.push(count)
-  }
-  	return rez[0]
+	let n = 2;
+	let arr = []
+	while (n <= base) {
+		if (base % n === 0) {
+			arr.unshift(n)
+			base = Math.floor(base / n);
+		} else n++;
+	}
+	let count = 0
+	let rez = []
+	for (let i = 0; i < arr.length; i++) {
+		let del = arr[i]
+		while (del <= number) {
+			count += Math.floor(number / del)
+			del *= arr[i]
+		}
+		rez.push(count)
+	}
+
+	var quant = 1
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] == arr[i + 1]) {
+			quant++
+		} else break;
+	}
+
+	if (quant != 1) {
+		return Math.floor(rez[0] / quant)
+	} else {
+		return rez[0]
+	}
+
 }
